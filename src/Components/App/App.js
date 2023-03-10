be390634d2bec56b5968e1f80144ba33186988a8
Erkan <12980@coderacademy.edu.au>
2023-03-10 17:32:39 +1100

    fixed readme and Spotify.js to produce the required result

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,16 +3,16 @@ import './App.css'
 import SearchBar from '../SearchBar/SearchBar'
 import SearchResults from '../SearchResults/SearchResults'
 import PlayList from '../PlayList/PlayList'
+import Spotify from '../../util/Spotify'
 // import TrackList from '../TrackList/TrackList'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
-      { name: 'name2', artist: 'artist2', album: 'album2', id: 2 }, { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }],
-      playListName: 'My Playlist', playListTracks: [{ name: 'name4', artist: 'artist4', album: 'album4', id: 4 },
-      { name: 'name5', artist: 'artist5', album: 'album5', id: 5 }, { name: 'name6', artist: 'artist6', album: 'album6', id: 6 }]
+      searchResults: [],
+      playListName: 'My Playlist', 
+      playListTracks: []
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
@@ -23,11 +23,17 @@ class App extends React.Component {
 
   savePlaylist() {
     // alert('this method works!')
-    const trackURIs = this.state.playListTracks.map(track => track.uri);
+    const trackUris = this.state.playListTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playListName, trackUris).then(() => {
+      this.setState({
+        playListName: 'New Playlist',
+        playListTracks: []
+      })
+    })
   }
 
   updatePlayListName(name) {
-    this.setState({playListName: name})
+    this.setState({ playListName: name })
   }
 
   removeTrack(track) {
@@ -47,7 +53,9 @@ class App extends React.Component {
   }
 
   search(searchTerm) {
-    console.log(searchTerm);
+    Spotify.search(searchTerm).then(searchResults => {
+      this.setState({ searchResults: searchResults });
+    });
   }
 
   render() {
@@ -55,7 +63,7 @@ class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar onSearch={this.search}/>
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <PlayList playListName={this.state.playListName} playListTracks={this.state.playListTracks} onRemove={this.removeTrack} onNameChange={this.updatePlayListName} onSave={this.savePlayList} />
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -8,7 +8,7 @@ class SearchBar extends React.Component {
         this.state = {
             searchTerm: ''
         }
-        
+
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
     }
@@ -20,11 +20,12 @@ class SearchBar extends React.Component {
     search() {
         this.props.onSearch(this.state.searchTerm);
     }
+    
     render() {
         return (
             <div className="SearchBar">
                 <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
-                <button className="SearchButton">SEARCH</button>
+                <button onClick={this.search} className="SearchButton">SEARCH</button>
             </div>
         )
     }
